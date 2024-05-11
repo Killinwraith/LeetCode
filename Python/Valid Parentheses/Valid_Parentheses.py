429c68071f0582d9340548c73d3c15e938983440
Hetang Mehta <hetangmehta03@gmail.com>
2024-05-10 22:14:14 -0600

    finished leetcode: 20. Valid Parentheses

--- Python/Valid Parentheses/Valid_Parentheses.py
+++ Python/Valid Parentheses/Valid_Parentheses.py
@@ -5,29 +5,32 @@ def isValid(s):
     :rtype: bool
     '''
  
+    opening = {'(', '[', '{'}
+    closing = {')': '(', ']': '[', '}': '{'}
+    
     stack = []
     total = False
     if len(s) == 1:
         return False
-    
-    for letter in s:
-        if letter in ("(","{","["):
-            stack.append(letter)
-        else:
-            firstToClose = stack.pop()
-            if (letter == (")") and firstToClose == ("(")) or (letter == ("}") and firstToClose == ("{")) or (letter == ("]") and firstToClose == ("[")):
-                total = True
+    if s[0] in closing:
+        return False
+
+    for parenthesis in s:
+        if parenthesis in opening:
+            stack.append(parenthesis)
+        elif parenthesis in closing:
+            if len(stack) > 0:
+                if stack.pop() != closing[parenthesis]:
+                    return False
+                else:
+                    total = True
             else:
-                return False
-    
+                total = False
     return total
 
-"""
-currenlty failling....I need to fix this some how ....
-"""
-
 print('Test 1: input = \'()\' - ',isValid('()'))
-print('Test 1: input = \'((\' - ',isValid('(('))
-print('Test 2: input = \'()[]{}\' - ',isValid('()[]{}'))
-print('Test 3: input = \'(}\' - ',isValid('(}'))
-print('Test 4: input = \'([)]\' - ',isValid('([)]'))
+print('Test 2: input = \'((\' - ',isValid('(('))
+print('Test 3: input = \'()[]{}\' - ',isValid('()[]{}'))
+print('Test 4: input = \'(}\' - ',isValid('(}'))
+print('Test 5: input = \'([)]\' - ',isValid('([)]')) 
+print('Test 6: input = \'(){}}{\' - ',isValid('(){}}{')) 
